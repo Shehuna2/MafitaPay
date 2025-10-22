4dfab2716ec46255b9341934165aa98a8a3ea980
Shehuna2 <arbamiyya@gmail.com>
2025-10-22 14:43:49 +0100

    Add fix email verification

--- backend/mafitapay/settings.py
+++ backend/mafitapay/settings.py
@@ -70,7 +70,7 @@ BASE_URL = os.getenv("BASE_URL")
 
 # Email (Gmail SMTP)
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
