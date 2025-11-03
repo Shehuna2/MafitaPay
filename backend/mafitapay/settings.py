9cae921db43ec8c568b15bbdf1f2bc683a4714ea
Shehuna2 <arbamiyya@gmail.com>
2025-11-03 21:30:45 +0100

    Replace gmail to sendgrid

--- backend/mafitapay/settings.py
+++ backend/mafitapay/settings.py
@@ -179,7 +179,7 @@ EMAIL_BACKEND = (
     if DEBUG
     else os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 )
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.sendgrid.net")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
